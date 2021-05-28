8c5a67b90a2c8e3f8ff135348ea7ff78aea25d49
root <root@0bbc87aacb31>
2021-05-28 23:59:02 +0000

    My ages done badgers in progress.

---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,14 @@
+package _05_for_loops._2_badgers;
+
+public class Badgers {
+	
+	int badgerCount = 0; {
+	for (badgerCount=0; badgerCount<=12; badgerCount+=1 ) {
+		
+		System.out.print("badger");
+		
+}
+
+}
+
+}
